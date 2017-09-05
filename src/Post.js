fa696ec02dbacde5c398583c5aeeffb2078a7c76
Dmitry Sedov <dmsedov92@gmail.com>
2017-09-05 18:37:25 +0300

    fix  this.id property (Post.js)

--- src/Post.js
+++ src/Post.js
@@ -1,7 +1,7 @@
 export default class Post {
   static id = 1;
   constructor(title, body) {
-    this.id = Post.id + 1;
+    this.id = Post.id++;
     this.title = title;
     this.body = body;
   }
