972bc04e036f125ef8e3618962f0b3423991cdf0
Dmitry Sedov <dmsedov92@gmail.com>
2017-09-05 13:59:36 +0300

    add test module

---
+++ _tests_/server.test.js
@@ -0,0 +1,39 @@
+import request from 'supertest';
+import server from '../index';
+
+describe('request', () => {
+  it('GET /', async () => {
+    const res = await request(server()).get('/');
+    expect(res.status).toBe(200);
+  });
+
+  it('GET /posts', async () => {
+    const res = await request(server()).get('/posts');
+    expect(res.status).toBe(200);
+  });
+
+  it('GET /posts/new', async () => {
+    const res = await request(server()).get('/posts/newPost');
+    expect(res.status).toBe(200);
+  });
+
+  it('POST /posts', async () => {
+    const res = await request(server()).post('/posts')
+    .type('form').send({ title: 'title', body: 'body' });
+    expect(res.status).toBe(302);
+  });
+
+  it('POST /posts without query string', async () => {
+    const res = await request(server()).post('/posts');
+    expect(res.status).toBe(422);
+  });
+
+  it('GET /posts/:id', async () => {
+    const query = request(server());
+    const res1 = await query.post('/posts')
+    .type('form').send({ title: 'title', body: 'body' });
+    expect(res1.status).toBe(302);
+    const res2 = await query.get(res1.headers.location);
+    expect(res2.status).toBe(200);
+  });
+});
