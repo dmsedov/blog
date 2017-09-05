bca650ad74ba2ca32f2fa050f0922a83dfa7e38e
Dmitry Sedov <dmsedov92@gmail.com>
2017-09-05 18:36:09 +0300

    add "GET /posts" handler (index.js)

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 import Express from 'express';
 import bodyParser from 'body-parser';
+import Router from 'named-routes';
 import Post from './src/Post';
 
 export default () => {
-  const app = new Express();
+  const app = Express();
+  const router = new Router();
+  router.extendExpress(app);
+  router.registerAppHelpers(app);
   app.set('view engine', 'pug');
   app.use(bodyParser.urlencoded({ extended: false }));
-  let ListOfPosts = [new Post('first title', 'content1'),
+  const ListOfPosts = [new Post('first title', 'content1'),
     new Post('second title', 'content2')];
-  app.get('/', (req, res) => {
+  app.get('/', 'root', (req, res) => {
+    const url = app.namedRoutes.build('root');
     res.render('index');
   });
 
-  // app.get('/posts', (req, res) => {
-  //   res.render()
-  // });
+  app.get('/posts', 'Posts list', (req, res) => {
+    const url = app.namedRoutes.build('Posts list');
+    res.render('Posts/listOfPosts', { url, posts: ListOfPosts });
+  });
   //
   // app.get('/posts/:id', (req, res) => {
   //   res.render('Posts/')
