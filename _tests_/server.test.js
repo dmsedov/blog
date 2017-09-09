1a7d7af81e4cb65f823a9a64118259f31703178c
Dmitry Sedov <dmsedov92@gmail.com>
2017-09-10 00:30:32 +0300

    add tests for "PATCH with 422" and "DELETE" methods

--- _tests_/server.test.js
+++ _tests_/server.test.js
@@ -38,11 +38,11 @@ describe('request', () => {
   });
 
   it('GET /posts/:id/edit', async () => {
-    const query = request(server());
-    const res1 = await query.post('/posts')
+    const app = server();
+    const res1 = await request(app).post('/posts')
     .type('form').send({ title: 'title', body: 'body' });
     expect(res1.status).toBe(302);
-    const res2 = await query.get(res1.headers.location);
+    const res2 = await request(app).get(res1.headers.location);
     expect(res2.status).toBe(200);
   });
 
@@ -51,8 +51,26 @@ describe('request', () => {
     const res1 = await request(app).post('/posts')
     .type('form').send({ title: 'title', body: 'body' });
     const url = res1.headers.location.split('/').slice(0, -1).join('/');
-    const res2 = await query.patch(`${url}`).type('form')
+    const res2 = await request(app).patch(url).type('form')
     .send({ title: 'newTitle', body: 'newBody' });
+    expect(res2.status).toBe(302);
+  });
+
+  it('PATCH /posts/:id ("unprocessable entity" status 422)', async () => {
+    const app = server();
+    const res1 = await request(app).post('/posts')
+    .type('form').send({ title: 'title', body: 'body' });
+    const url = res1.headers.location.split('/').slice(0, -1).join('/');
+    const res2 = await request(app).patch(url).type('form').send({ title: 'newTitle' });
+    expect(res2.status).toBe(422);
+  });
+
+  it('DELETE /posts/:id', async () => {
+    const app = server();
+    const res1 = await request(app).post('/posts')
+    .type('form').send({ title: 'title', body: 'body' });
+    const url = res1.headers.location.split('/').slice(0, -1).join('/');
+    const res2 = await request(app).delete(url);
     expect(res2.status).toBe(302);
   });
 });
