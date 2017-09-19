1d1b558b847c6de98b70c3403f364cdfe089e8a1
Dmitry Sedov <dmsedov92@gmail.com>
2017-09-19 17:06:07 +0300

    add module ORM sequelize

---
+++ models/index.js
@@ -0,0 +1,28 @@
+import Sequelize from 'sequelize';
+import fs from 'fs';
+import path from 'path';
+
+const env = process.env.NODE_ENV || 'development';
+const config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
+const db = {};
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+fs.readdirSync(__dirname)
+    .filter(file => file.indexOf('.') !== 0 && file !== 'index.js')
+    .forEach((file) => {
+      const model = sequelize.import(path.join(__dirname, file));
+      console.log(model.name, model, '!!! modelname, model');
+      db[model.name] = model;
+    });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
